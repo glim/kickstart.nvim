e2026e105dafb201481f9d1c9f578519ffcffc29
glim <451000+glim@users.noreply.github.com>
2023-09-11 21:57:18 -0400

    ctrl l is back

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -16,7 +16,7 @@ return {
       vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
       -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
       -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-      vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
       -- vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
     end
 
